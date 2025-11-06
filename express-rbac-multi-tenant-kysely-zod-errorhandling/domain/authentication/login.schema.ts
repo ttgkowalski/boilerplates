23a2c9b5488e789ba8324a5d8d18f880f753abed
kennedfer <kenned14santos@gmail.com>
2025-11-06 11:19:18 -0300

    fix: Fix Zod deprecated email validation and set the max password length to bcrypt limit

--- express-rbac-multi-tenant-kysely-zod-errorhandling/domain/authentication/login.schema.ts
+++ express-rbac-multi-tenant-kysely-zod-errorhandling/domain/authentication/login.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
-  });
-  
+  email: z.email(), // z.string().email() is deprecated
+  // bcrypt uses only the first 71 chars; the rest are ignored
+  password: z.string().min(8).max(70),
+});
--- express-rbac-multi-tenant-kysely-zod-errorhandling/domain/user/update-user.schema.ts
+++ express-rbac-multi-tenant-kysely-zod-errorhandling/domain/user/update-user.schema.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 export const updateUserSchema = z.object({
     tenant_id: z.string().nullable().optional(),
-    email: z.string().email().optional(),
-    password: z.string().min(1).optional(),
+    email: z.email().optional(),// z.string().email() is deprecated
+    // bcrypt uses only the first 71 chars; the rest are ignored
+    password: z.string().min(1).max(70).optional(),
     role: z.enum(["User"]).optional(),
   }).refine((obj) => Object.keys(obj).length > 0, { message: "At least one field required" });
